ee9a38c516e1253bab3630584ffe00c585a3f2f2
Unknown <pqh5113@gmail.com>
2018-11-14 20:24:54 -0500

    nice-ify

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import requests, os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
@@ -20,7 +20,7 @@ def get_file_list():
     output +="</ul>"
     return output
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/u', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -36,21 +36,15 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
+            return redirect(url_for('upload_file',
                                     filename=filename))
-    return '''
-    <!doctype html>
-    <title>Phishing Boat</title>
-    <h1>Phishing Boat</h1>
-    <h2>Upload an email for analysis</h2>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file><i style="color:red">Allowed types: .eml, .msg, .txt</i><br>
-      <input type=submit value=Upload>
-    </form>
-    <br><h2>Files</h2><br>
-    '''+get_file_list()
+    return render_template("upload.html",files=get_file_list())
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
